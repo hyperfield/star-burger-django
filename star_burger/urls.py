272daba1c6475de274a0acdbea05738635a3ceea
django-serv Hyperfield <sequence@myrealm.ca>
2022-03-12 18:19:21 +0000

    Setup rollbar

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('api/', include('foodcartapp.urls')),
     path('manager/', include('restaurateur.urls')),
     path('api-auth/', include('rest_framework.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
