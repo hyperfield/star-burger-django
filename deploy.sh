9f79a8d20a2d61149d7636d808576114fa8752a0
Sequence <sequence@myrealm.ca>
2022-11-28 22:48:09 +0100

    Fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-if ![git pull && [ ! -d "venv" ] && [ python -m venv venv ] && \
+if !(git pull && [ ! -d "venv" ] && [ python -m venv venv ] && \
    [ source venv/bin/activate ] && [ pip install -r requirements.txt ] && \
    [ python manage.py migrate ] && [ npm ci --dev ] && \
    [ ./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./" ] \
-    && [ python manage.py collectstatic ] && [ systemctl restart star-burger ]];
+    && [ python manage.py collectstatic ] && [ systemctl restart star-burger ]);
 then
   exit_code=$?
-  echo "Could not activate Python virtual environment: exit code $exit_code" >&2
+  echo "Something went wrong: exit code $exit_code" >&2
   exit $exit_code
 fi
 
-deactivate;
+deactivate
 
 git_commit_id=$(git rev-parse --short HEAD)
 rb_key=$(grep -i 'ROLLBAR_KEY=' /etc/systemd/system/star-burger.env | cut -f2 -d'=' | cut -f2 -d'"')
